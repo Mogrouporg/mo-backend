260d9cb8c9b7793821c5f57b3d54f7904ba4b45e
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2023-03-27 11:21:03 +0100

    Started the auth, otp utilities

--- src/utils/otp.util.js
+++ src/utils/otp.util.js
@@ -4,7 +4,27 @@ const {User} = require('../models/users.model')
 exports.genOtp = async (email) => {
     const secret = speakeasy.generateSecret({length: 20});
     const savedSecret = secret.base32;
+    const otp = speakeasy.totp({
+        secret: savedSecret,
+        encoding: 'base32'
+    });
     await User.findOneAndUpdate({email: email}, {
-        otpSecret: savedSecret
+        otpSecret: savedSecret,
     }, {new: true})
+    return otp;
+}
+
+exports.verifyOtp = async (user, body, next)=>{
+    const findUser = User.findOne({email: user});
+    const tokenSecret = findUser.otpSecret;
+    const isVerified = speakeasy.totp.verify({
+        secret: tokenSecret,
+        token: body,
+        encoding: 'base32',
+        window: 6
+    });
+    if(isVerified === true){
+        findUser.isVerified = true;
+    }
+    next()
 }
