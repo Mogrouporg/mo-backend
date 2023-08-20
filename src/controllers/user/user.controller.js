6e61b304f134298e8c5862c2317cb0deef983891
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2023-08-20 21:40:26 +0100

    fixing

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -3,6 +3,7 @@ const {verifyToken} = require("../../utils/updateToken.utils");
 const { findNotifById } = require('../../services/notif/notif.services');
 const {deposit, verifyDeposit, investInRealEstate, sellRealEstateInvestment, investInTransport, sellTransportInvestment, withdrawFunds} = require("../../services/users/finance.services");
 const {myProfile, getMyTransactions, getSingleRealEstateInvestment, getSingleTransInvestment} = require("../../services/users/account.service");
+const { getAllRealInvestments, getAllTransInvestments } = require('../../services/admin/admin.service');
 
 useRouter.get('/user/notifications', verifyToken, findNotifById);
 //Payments && Balance
@@ -17,4 +18,6 @@ useRouter.post('/transport/:id/sell', verifyToken, sellTransportInvestment);
 useRouter.post('/account/withdraw', verifyToken, withdrawFunds);
 useRouter.get('/real-estate/:id', verifyToken, getSingleRealEstateInvestment);
 useRouter.get('/transport/:id', verifyToken, getSingleTransInvestment);
+useRouter.get('/real-estate', verifyToken, getAllRealInvestments);
+useRouter.get('/transport', verifyToken, getAllTransInvestments);
 module.exports = useRouter;
