64dc87a4de38cd5afd34177f27b8b6d7c19269f5
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2023-12-09 14:41:39 -0500

    Update Paystack authorization key

--- src/utils/payment.utils.js
+++ src/utils/payment.utils.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const paystack = axios.create({
   baseURL: 'https://api.paystack.co',
   headers: {
-    Authorization: process.env.PAYSTACK_SECRET_KEY,
+    Authorization: process.env.PAYSTACK_PUBLIC_KEY,
     'Content-Type': 'application/json',
     'cache-control': "no-cache"
   },
