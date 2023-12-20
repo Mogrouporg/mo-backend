15d20713be16a8449d358534d00c20e633c7b46e
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2023-12-20 17:45:13 -0500

    mailing

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,37 +1,37 @@
 const nodemailer = require('nodemailer');
-require('dotenv').config()
-const sendMail =  async(options)=>{
-    const  transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'mailmogroupltd@gmail.com',
-            pass: process.env.PASS_TEST
-        }
-    });
+require('dotenv').config();
 
-    let message = {
-        from: 'MO Group <info@mogroupltd.com>',
-        to: options.email,
-        subject: options.subject,
-        html: options.html
-    }
+// Create the transporter outside of the sendMail function
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  pool: true,
+  auth: {
+    user: 'mailmogroupltd@gmail.com',
+    pass: process.env.PASS_TEST
+  }
+});
 
-     const info = await transporter.sendMail(message)
-}
+const sendMail = async (options) => {
+  let message = {
+    from: 'MO Group <info@mogroupltd.com>',
+    to: options.email,
+    subject: options.subject,
+    html: options.html
+  };
 
-/*
-sendMail({
-    to: "oluafemi07@gmail.com",
-    subject: "Testing",
-    text: "Hello Chris"
-}).then(r => {
-    console.log("Worked")
-}).catch(e=>{
-    console.log(e)
-})
-*/
+  try {
+    const info = await transporter.sendMail(message);
+    return info;
+  } catch (error) {
+    // Handle the error appropriately
+    console.error('Error sending email:', error);
+    throw error; // Rethrow or handle the error as needed
+  }
+};
 
-
+// Export the sendMail function and the transporter
 module.exports = {
-    sendMail
-}
+  sendMail,
+  transporter
+};
+
--- src/utils/notifyAllUsers.util.js
+++ src/utils/notifyAllUsers.util.js
@@ -1,47 +1,49 @@
-const { pushNotification } = require('../services/notif/notif.services')
-const {sendMail} = require("./mailer");
+const {pushNotification} = require('../services/notif/notif.services')
+const {sendMail, transporter} = require("./mailer");
 
 
 const BATCH_SIZE = 50;
 const DELAY_BETWEEN_BATCHES = 5000;
 
 async function sendMailBatch(emailBatch, title, message) {
-    return Promise.all(emailBatch.map(email => sendMail({
-        email,
-        subject: title,
-        html: message
-    })));
+  return Promise.all(emailBatch.map(email => sendMail({
+    email,
+    subject: title,
+    html: message
+  })));
 }
 
 async function pushNotificationBatch(emailBatch, message) {
-    return Promise.all(emailBatch.map(email => pushNotification({
-        email,
-        message
-    })));
+  return Promise.all(emailBatch.map(email => pushNotification({
+    email,
+    message
+  })));
 }
 
 exports.notifyAllUsers = async (users, title, message) => {
-    try {
-        const emails = users.map(user => user);
-        
-        for (let i = 0; i < emails.length; i += BATCH_SIZE) {
-            const emailBatch = emails.slice(i, i + BATCH_SIZE);
-            
-            // Use Promise.all to send emails and notifications in parallel
-            await Promise.all([
-                sendMailBatch(emailBatch, title, message),
-                pushNotificationBatch(emailBatch, message)
-            ]);
+  try {
+    const emails = users.map(user => user);
 
-            // Introduce a delay between batches
-            if (i + BATCH_SIZE < emails.length) {
-                await new Promise(resolve => setTimeout(resolve, DELAY_BETWEEN_BATCHES));
-            }
-        }
+    for (let i = 0; i < emails.length; i += BATCH_SIZE) {
+      const emailBatch = emails.slice(i, i + BATCH_SIZE);
 
-        return true;
+      // Use Promise.all to send emails and notifications in parallel
+      await Promise.all([
+        sendMailBatch(emailBatch, title, message),
+        pushNotificationBatch(emailBatch, message)
+      ]);
 
-    } catch (error) {
-        console.log(error);
+      // Introduce a delay between batches
+      if (i + BATCH_SIZE < emails.length) {
+        await new Promise(resolve => setTimeout(resolve, DELAY_BETWEEN_BATCHES));
+      }
     }
+
+    return true;
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    transporter.close();
+  }
 }
